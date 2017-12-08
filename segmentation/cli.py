b9da6b9fd6e3f7cefd200430d463a4f2c6fa1ace
sofroniewn <sofroniewn@gmail.com>
2017-12-08 14:24:03 -0800

    fix evaluate command

--- segmentation/cli.py
+++ segmentation/cli.py
@@ -2,6 +2,7 @@ import click
 
 settings = dict(help_option_names=['-h', '--help'])
 from segmentation.commands.train import train_command
+from segmentation.commands.eval import evaluate_command
 
 @click.group(options_metavar='', subcommand_metavar='<command>', context_settings=settings)
 def cli():
@@ -11,3 +12,4 @@ def cli():
     """
 
 cli.add_command(train_command)
+cli.add_command(evaluate_command)
